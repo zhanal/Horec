965a841868c65c7588e66ba1cf0306fb1c6f7ac1
zhanal <113106850+zhanal@users.noreply.github.com>
2022-12-23 14:35:23 +0100

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import PerformanceView
-
+from catalog.views import TempPageView
 app_name='projects'
 
 urlpatterns = [
-    path('', PerformanceView, name='performance'),
+#     path('', PerformanceView, name='performance'),
+    path('', TempPageView.as_view(), name='performance'),
 ]
